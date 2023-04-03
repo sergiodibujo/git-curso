d144114e71e63ae8624c89d360938f94911d66b2
¨sergiodibujo¨ <sergiodibujo@gmail.com>
2023-04-03 07:58:23 -0500

    Se crean los talleres

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
   let sarLogo = "https://logoeps.com/wp-content/uploads/2013/03/jurassic-park-vector-logo.png";
   // Hago el map, Luego pregunto quien esta y luego pinto
   // let autorizado = true;
-  
-  
+
+
   let users = ['Sergio', "Andres", "Jairo"];
-  
+
   //  Cambiar autorizado por  El valor del input cuando escribe";
 
   let autorizado = "Andres";
   if (autorizado.includes("Sergio")) {
     autorizado = "Este es el admin";
-  } 
+  }
   else if (autorizado.includes("Jairo")) {
     autorizado = "Este es el hombre gordo";
   }
@@ -46,13 +46,30 @@ function App() {
             <input type="text" id="userName"></input>
           </div>
           <p className="sarUsuario">{autorizado}</p>
-          
+
         </div>
       </div>
+      <div>Dia 2git status
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <ul className="floo">
+        <li className="lista" id="faa1">trxto 1</li>
+        <li className="lista" id="faa2">trxto 2</li>
+        <li className="lista" id="faa3">trxto 3</li>
+        <li className="lista" id="faa4">trxto 4</li>
+        <li className="lista" id="faa5">trxto 5</li>
+        <li className="lista" id="faa6">trxto 6</li>
+      </ul>
+    <p>COmo lo muevo arriba y abajo? ALT + Flecha arriba</p>
+      <div>Shift+alt+F Reindenta el codigo</div>
 
 
-
-
+    <p className="">Duplico linea con SHift+Alt+flecha arriba</p>
+    <p>Listo emmet y listo mover, solo es entrenarlo</p>
 
 
       <div className="App">
